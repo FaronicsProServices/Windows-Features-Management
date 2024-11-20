9e78d94eee98018997de3d862b29b0c8152671a1
FaronicsProServices <proservices@faronics.com>
2024-11-21 02:17:10 +0530

    Create FetchInfoforSpecificFeature.ps1

---
+++ OptionalFeatures/FetchInfoforSpecificFeature.ps1
@@ -0,0 +1,4 @@
+# Check the status of a specific Windows optional feature
+# Replace "*Feature-Name*" with the name of the feature you want to query, e.g., "Microsoft-Windows-Subsystem-Linux"
+# get-windowsoptionalfeature -online -featurename *Microsoft-Hyper-V-All*
+get-windowsoptionalfeature -online -featurename *Feature-Name*
