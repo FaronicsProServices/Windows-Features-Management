20c7f08b36d81be0be7496d7d24ffe412f95ef3c
FaronicsProServices <proservices@faronics.com>
2024-11-21 02:19:57 +0530

    Update EnableSpecificWindowsOptionalFeature.ps1

--- OptionalFeatures/EnableSpecificWindowsOptionalFeature.ps1
+++ OptionalFeatures/EnableSpecificWindowsOptionalFeature.ps1
@@ -1,4 +1,3 @@
 # Enables a specific Optional feature on Windows such as Microsoft Subsystem for Linux
-#enable-windowsoptionalfeature -online -featureName Microsoft-Windows-Subsystem-Linux -all -NoRestart
 # enable-windowsoptionalfeature -online -featureName Microsoft-Windows-Subsystem-Linux -all -NoRestart
 enable-windowsoptionalfeature -online -featureName FeatureName -all -NoRestart
